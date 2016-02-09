e3e3570395749f8e6cbb34607580ef3894c39391
Steven Cook <stevenjamescook@gmail.com>
2016-02-09 10:19:28 +0000

    Update comma_service_spec.rb

--- spec/services/comma_service_spec.rb
+++ spec/services/comma_service_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe CommaService do
 
   context "input string is a floating point number" do
     describe "an input string containing 15 characters, a decimal point with a comma interval of 2" do
-      it "returns a string with 4 commas tot he left of the decimal point" do
+      it "returns a string with 4 commas to the left of the decimal point" do
         commafied_string = CommaService::commafied({interval: 2, string: "123456789.123456"})
         expect(commafied_string).to eql "1,23,45,67,89.123456"
       end
